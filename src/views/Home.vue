62c842aa1e7b90c3b627eafc04bdf9d3ae3532c6
Marat Oshmiansky <maratical@gmail.com>
2021-07-21 10:56:59 -0700

    initial commit, added user authentication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newPostsParams.title" />
+      Body:
+      <input type="text" v-model="newPostsParams.body" />
+      Image:
+      <input type="text" v-model="newPostsParams.image" />
+    </div>
+    <button v-on:click="createPost()">Create a new post!</button>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>Title: {{ post.title }}</h2>
+      <p>Body: {{ post.body }}</p>
+      <p>Image: {{ post.image }}</p>
+      <button v-on:click="showPost(post)">More info!</button>
+    </div>
+    <dialog id="post-details">
+      <form method="dialog">
+        <h1>Post Info!</h1>
+        <p>
+          Title:
+          <input type="text" v-model="currentPost.title" />
+        </p>
+        <p>
+          Body:
+          <input type="text" v-model="currentPost.body" />
+        </p>
+        <p>
+          Image:
+          <input type="text" v-model="currentPost.image" />
+        </p>
+        <button v-on:click="updatePost(currentPost)">Update</button>
+        <button v-on:click="destroyPost(currentPost)">Destroy</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to blog-frontend-app!",
+      posts: [],
+      newPostsParams: {},
+      currentPost: {},
+    };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      axios.get("http://localhost:3000/posts").then((response) => {
+        this.posts = response.data;
+        console.log("All posts:", this.posts);
+      });
+    },
+    createPost: function () {
+      console.log("Creating post");
+      axios
+        .post("http://localhost:3000/posts", this.newPostsParams)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.posts.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPost: function (post) {
+      console.log(post);
+      this.currentPost = post;
+      document.querySelector("#post-details").showModal();
+    },
+    updatePost: function (post) {
+      var editPostParams = post;
+      axios.patch("http://localhost:3000/posts/" + post.id, editPostParams).then((response) => {
+        console.log("Success!", response.data);
+      });
+    },
+    destroyPost: function (post) {
+      axios.delete("http://localhost:3000/posts/" + post.id).then((response) => {
+        console.log("Success!", response.data);
+        var index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+    },
   },
 };
 </script>
