3c803bd29c8626f0f326e8db045674b7653b5d83
Marat Oshmiansky <maratical@gmail.com>
2021-07-22 11:01:47 -0700

    add PostsShow.vue to show one post

---
+++ src/views/PostsShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="posts-show">
+    <div class="container">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+      <img v-bind:src="post.image" alt="post.title" />
+      <router-link to="/posts">Back to all posts</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      errors: [],
+      post: {},
+    };
+  },
+  created: function () {
+    axios.get("/posts/1").then((response) => {
+      this.post = response.data;
+    });
+  },
+};
+</script>
